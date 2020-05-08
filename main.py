db07ebb4094ae72280bb77fa1368aad99aac9289
Sergey <serg78kh@yandex.ru>
2020-05-08 21:38:08 +0300

    new commit

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from telegram.ext import CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 TOKEN = "852223716:AAGdVVMYgHTro5LM7yTfmprosTgE9cBTxOA"
+questions = []
+responses = []
 
 
 def start(update, context):
-    reply_keyboard = [["Готов"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    update.message.reply_text("Вас приветствует бот 'Тестирующая система', в нем вы можете пройти "
-                              "тест по знаниям соцсетей. Если вы готовы начать нажмите на кнопку "
-                              "'Готов'", reply_markup=markup)
+    global questions, responses
+    with open("static/json/bot.json", encoding='utf-8') as file:
+        reply_keyboard = [["Готов"]]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        update.message.reply_text(f"{file.readlines()} Вас приветствует бот "
+                                  f"'Тестирующая система', в нем вы можете пройти "
+                                  "тест по знаниям соцсетей. Если вы готовы начать нажмите на кнопку "
+                                  "'Готов'", reply_markup=markup)
 
 
 def test(update, context):
+    pass
+
+
+def stop(update, context):
     pass
 
 
@@ -23,6 +32,23 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.regex("Готов"),
                                   test))
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(Filters.text, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(Filters.text, second_response)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
 
