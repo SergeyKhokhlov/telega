87a60f0e820e92c3a4d99e80e6b9a6f516caa28e
Sergey <serg78kh@yandex.ru>
2020-05-08 23:59:26 +0300

    update json file v 12

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, ConversationHandler
 import json
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 TOKEN = "852223716:AAGdVVMYgHTro5LM7yTfmprosTgE9cBTxOA"
 questions = []
 responses = []
+correct_answers = 0
 
 
 def start(update, context):
@@ -16,7 +18,6 @@ def start(update, context):
         for i in test:
             questions.append(i['question'])
             responses.append(i['response'])
-        print(questions)
     reply_keyboard = [["Готов"]]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text("Вас приветствует бот 'Тестирующая система', "
@@ -26,12 +27,30 @@ def start(update, context):
                               "'Готов'", reply_markup=markup)
 
 
-def test(update, context):
-    pass
+def first_response(update, context):
+    global questions, responses, correct_answers
+    if update.message.text != "/stop":
+        update.message.reply_text(questions[0])
+        if update.message.text == responses[0]:
+            correct_answers += 1
+        return 2
+    else:
+        return ConversationHandler.END
+
+
+def second_response(update, context):
+    global questions, responses, correct_answers
+    if update.message.text != "/stop":
+        update.message.reply_text(questions[1])
+        if update.message.text == responses[1]:
+            correct_answers += 1
+        return 2
+    else:
+        return ConversationHandler.END
 
 
 def stop(update, context):
-    pass
+    return ConversationHandler.END
 
 
 def main():
@@ -39,24 +58,24 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.regex("Готов"),
-                                  test))
-    # conv_handler = ConversationHandler(
-    #     # Точка входа в диалог.
-    #     # В данном случае — команда /start. Она задаёт первый вопрос.
-    #     entry_points=[CommandHandler('start', start)],
-    #
-    #     # Состояние внутри диалога.
-    #     # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
-    #     states={
-    #         # Функция читает ответ на первый вопрос и задаёт второй.
-    #         1: [MessageHandler(Filters.text, first_response)],
-    #         # Функция читает ответ на второй вопрос и завершает диалог.
-    #         2: [MessageHandler(Filters.text, second_response)]
-    #     },
-    #     # Точка прерывания диалога. В данном случае — команда /stop.
-    #     fallbacks=[CommandHandler('stop', stop)]
-    # )
-    # dp.add_handler(conv_handler)
+                                  first_response))
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(Filters.text, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(Filters.text, second_response)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
 
